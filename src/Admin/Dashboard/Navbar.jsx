179d022572a9005d7d99243886eb3f8f09062b10
fetaliyevajale <fetaliyevajale@gmail.com>
2024-11-11 13:29:52 -0800

    navbar/lagout

--- src/Admin/Dashboard/Navbar.jsx
+++ src/Admin/Dashboard/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';  // React Router-dan useNavigate hook-u
 
 export default function Navbar() {
+  const navigate = useNavigate(); // Navigate funksiyasını çağırırıq
+
+  // Logout funksiyası
+  const handleLogout = () => {
+    localStorage.removeItem('token');  // Tokeni localStorage-dan silirik
+    navigate('/login');  // İstifadəçini login səhifəsinə yönləndiririk
+  };
+
   return (
-    <div className='navbar'>
-        <button>Logout</button>
+    <div className="navbar">
+      <button onClick={handleLogout}>Logout</button>  {/* Logout düyməsi */}
     </div>
-  )
+  );
 }
